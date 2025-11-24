f94b0b6ec2dbda3b9c08fa9aaae90de5eec97d45
llikian <pokillian64@gmail.com>
2025-11-25 00:03:19 +0100

    A few tests for Array2D (and by extension Array)

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,28 @@
 
 #include <iostream>
 #include <stdexcept>
+#include "Array2D.hpp"
 
 int main() {
     try {
+        Array2D<int> a1;
+        std::cout << a1 << '\n';
+
+        Array2D<int> a2(5, 3);
+        std::cout << a2 << '\n';
+
+        Array2D<int> a3(3, 5, 5);
+        std::cout << a3 << '\n';
+        a3.fill(69);
+        std::cout << a3 << '\n';
+        a3.assign(2, 2, 0);
+        std::cout << a3 << '\n';
+        a3.assign(4, 4, 3);
+        std::cout << a3 << '\n';
+        a3.resize(3, 3);
+        std::cout << a3 << '\n';
+        a3.resize(4, 4);
+        std::cout << a3 << '\n';
 
     } catch(const std::exception& exception) {
         std::cerr << "ERROR : " << exception.what() << '\n';
